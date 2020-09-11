50e7e36689b701bc4f48c53425a6f5e804f82513
Gabriel Pereira <gabriel.pereira@galileulog.srv.br>
2020-09-11 19:50:55 -0300

    Template

--- src/app/pages/public/public-template/public-template.component.ts
+++ src/app/pages/public/public-template/public-template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-public-template',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicTemplateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    if(this.router.url === '/entrar') {
+      this.router.navigateByUrl('/entrar/login');
+    }
   }
 
 }
